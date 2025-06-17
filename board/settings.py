d7632f5ffc96e39c58857de8e9ce040ed4ed5856
reserage <153280420+reserage@users.noreply.github.com>
2025-06-17 15:06:54 +0800

    Update settings.py

--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ca6cub8+^@o7oi=@e6+q6gbko#ogjve18q5mviy6qdvb9d6!t)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
